95bace5421bcf9285208dcc28b3b269fa9ef964f
Ram Mendez <balaramji95@gmail.com>
2024-09-17 14:33:07 +0200

    added routes, deleted some outputs for service sharing data. app done

--- src/app/services/sharing-data.service.ts
+++ src/app/services/sharing-data.service.ts
@@ -1,15 +1,22 @@
-import {EventEmitter, Injectable} from '@angular/core';
+import {EventEmitter, Injectable, Output} from '@angular/core';
+import {Product} from "../models/product";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharingDataService {
   private _idProductEventEmitter = new EventEmitter();
+  private _productEventEmitter: EventEmitter<Product> = new EventEmitter();
+
 
   constructor() {
   }
 
   get idProductEventEmitter() {
     return this._idProductEventEmitter;
+  }
+
+  get productEventEmitter() {
+    return this._productEventEmitter;
   }
 }
